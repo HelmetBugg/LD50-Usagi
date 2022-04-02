9cb552e9b3e0937065630271bcd9cb2f7448d648
HelmetBug <Powpengo@gmail.com>
2022-04-01 22:40:10 -0400

    added keyboard to main

--- src/main.js
+++ src/main.js
@@ -1,56 +1,74 @@
 "use strict";
-var g = hexi(512, 512, setup);
-g.scaleToWindow();
+var h = hexi(512, 512, setup);
+h.scaleToWindow();
 var ball = undefined;
-g.start();
+h.start();
 
 //The `setup` function to initialize your application
 function setup() {
 
   //Make a ball sprite.
   //circle arguments: diameter, fillStyle, strokeStyle, lineWidth, x, y
-  ball = g.circle(64, "powderBlue", "black", 2, 192, 256);
+  ball = h.circle(64, "powderBlue", "black", 2, 192, 256);
 
-  //Set the ball's velocity to 0
-  ball.vx = g.randomInt(5, 15);
-  ball.vy = g.randomInt(5, 15);
+  initKeyboard();
 
-  //Physics properties
-  ball.gravity = 0.3;
-  ball.frictionX = 1;
-  ball.frictionY = 0;
-  ball.mass = 1.3;
-
-  //Acceleration and friction properties
-  ball.accelerationX = 0.2;
-  ball.accelerationY = -0.2;
-  ball.frictionX = 1;
-  ball.frictionY = 1;
-
-  g.pointer.tap = function () {
-    ball.x = g.pointer.x - ball.halfWidth;
-    ball.y = g.pointer.y - ball.halfHeight;
-    ball.vx = g.randomInt(-15, 15);
-    ball.vy = g.randomInt(-15, 15);
-  };
-
-  var message = g.text("Tap to give the ball a new random velocity", "18px Futura", "black", 6, 6);
-  g.state = play;
+  h.state=play;
 }
 
 function play() {
-
-  ball.vy += ball.gravity;
-  ball.vx *= ball.frictionX;
-  ball.x += ball.vx;
-  ball.y += ball.vy;
-
-  var collision = g.contain(ball, g.stage, true);
-  if (collision) {
-    if (collision.has("bottom")) {
-      ball.frictionX = 0.98;
-    } else {
-      ball.frictionX = 1;
-    }
-  }
+h.move(ball);
+ 
 }
+
+
+function initKeyboard() {
+	let speed = 16;
+  let wKey = h.keyboard(87);
+  let sKey = h.keyboard(83);
+  let dKey = h.keyboard(68);
+  let aKey = h.keyboard(65);
+
+//	dialogue.press = () => {
+	
+//	}
+
+  wKey.press = () => {
+    ball.vy = -5;
+    ball.vx = 0;
+  };
+  wKey.release = () => {
+    ball.vy = 0;
+    ball.vx = 0;
+  };
+
+  sKey.press = () => {
+    ball.vy = 5;
+    ball.vx = 0;
+  };
+  sKey.release = () => {
+    ball.vy =0;
+    ball.vx = 0;
+  };
+
+  dKey.press = () => {
+    ball.vy = 0;
+    ball.vx = 5;
+  };
+  dKey.release = () => {
+    ball.vy =0;
+    ball.vx = 0;
+  };
+
+  aKey.press = () => {
+    ball.vy = 0;
+    ball.vx = -5;
+  };
+  aKey.release = () => {
+    ball.vy = 0;
+    ball.vx = 0;
+  };
+}
+
+
+
